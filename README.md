78d49c8de18f5efb0f324efb4a6bdf15dc65092a
Ramin Hakimi <r.hakimi@farakav.com>
2020-01-22 10:56:18 +0330

    First Public Ver.

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# zabbix-total-bw
+# Zabbix Total Bandwidth
+
+This script help you to get server bandwidth Average from zabbix database with API.  script result will be Total Month Bandwidth.
+
+just you must enter Start Date, End Date, IP Address and NIC name. 
+
+
+
+## Prerequisites
+
+- Zabbix Server with API enabled >= 3.x 
+- jq Linux Package (`sudo apt-get install jq`)
+- bc Linux Package (`sudo apt-get install bc`)
+- curl Linux Package (`sudo apt-get install curl`)
+
+
+
+## Tested:
+- Zabbix Server 4.4.2
+- Ubuntu 18.04.3 LTS
+
+
+
+## Usage
+
+**Note:** Start and end dates must be 30 days. The calculations of this script are based on 30 days
+
+**S_DATE**: Enter Start Date
+
+**E_DATE:** Enter End Date
+
+**ZBX_API_URL**: Zabbix API URL
+
+**ZBX_USER:** Zabbix API Username
+
+**ZBX_PASS**: Zabbix API Password
+
+**HOST & NIC**: Enter Server IP and NIC name (Example `192.168.1.30|eth0` )
+
+
+
+## Installation
+
+First, clone the repository using git (recommended):
+
+```
+git clone https://github.com/Ramhm/zabbix-total-bw.git
+```
+
+or download the script manually using this command:
+
+```
+curl "https://github.com/Ramhm/zabbix-total-bw/master/zb_total_bw.sh" -o zb_total_bw.sh
+```
+
+Then give the execution permission to the script and run it:
+
+```
+ $chmod +x zb_total_bw.sh
+ $./zb_total_bw.sh
+```
+
+
+
+
+
+> in this script we run any curl command to get user authentication information, “user.login” method is  used in the JSON RPC query. The following shell script is used to get  user authentication information. To export avg of the TBW , the “history.get” method is used in the JSPN RPC queries.
---
+++ zb_total_bw.sh
@@ -0,0 +1,134 @@
+#################################
+## R.Hakimi
+## Zabbix Total Bandwidth
+## 21-01-2020 17:48
+################################
+
+S_DATE='11/03/2019 06:00:00' # Start DATE MM/DD/YYYY / Local Time Zone
+E_DATE='12/03/2020 06:00:00' # End DATE MM/DD/YYYY / Local Time Zone
+
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+ST_DATE=`date --date="$S_DATE" +"%s"`
+ET_DATE=`date --date="$E_DATE" +"%s"`
+# TMP Files
+TMP_HISTORY_IN='.host_history_in'
+TMP_HISTORY_OUT='.host_history_out'
+TMP_HISTORY_SUM='.host_history_sum'
+# Result File
+BW_RESULT='Bandwidth_Result.csv'
+# Description
+HO='Hosts'
+NC='NIC'
+AVS='Avg Speed(MB)'
+TBH='Total Bandwidth Hours(MB)'
+TBD='Total Bandwidth Day(GB)'
+TBM='Total Bandwidth Month(TB)'
+SD='Start Date'
+ED='End Date'
+# Zabbix Config
+ZBX_API_URL='https://ZB_API_URL/api_jsonrpc.php'
+ZBX_USER='ZB_API_USER'
+ZBX_PASS='ZB_API_PASSWORD'
+# Hosts
+HOST_NIC='      XXX.XXX.XXX.XXX|NIC_NAME
+                XXX.XXX.XXX.XXX|NIC_NAME
+'
+
+
+## Clean Files
+        > $TMP_HISTORY_IN && \
+        > $TMP_HISTORY_OUT && \
+        > $TMP_HISTORY_SUM && \
+        > $BW_RESULT && \
+        echo -e "$HO,$NC,$AVS,$TBH,$TBD,$TBM,$SD,$ED" >> $BW_RESULT
+
+## TOKEN
+        TOKEN=$(curl -s -i -X POST \
+        -H 'Content-Type: application/json-rpc' \
+        -d '{ "params": { "user": "'$ZBX_USER'", "password": "'$ZBX_PASS'" }, "jsonrpc": "2.0", "method": "user.login", "id": 0 }' \
+        $ZBX_API_URL \
+        | grep 'jsonrpc' \
+        | jq '.result' \
+        | sed 's/[^0-9\|a-Z]*//g')
+
+## HOST ID / ITEM ID
+for HOSTS_D in $HOST_NIC
+do
+        HOST_IP=$(echo $HOSTS_D | cut -d"|" -f1)
+        HOST_NIC=$(echo $HOSTS_D | cut -d"|" -f2)
+
+        HOST_ID=$(curl -s -i -X POST \
+        -H 'Content-Type: application/json' \
+        -d '{"jsonrpc":"2.0","method":"host.get","params":{"output":"extend","filter":{"host":"'$HOST_IP'"}},"auth":"'$TOKEN'","id":1}' \
+        $ZBX_API_URL \
+        | grep 'jsonrpc' \
+        | jq '.result[].hostid' \
+        | sed 's/[^0-9\|a-Z]*//g')
+
+
+        ITEM_ID_IN=$(curl -s -i -X POST \
+        -H 'Content-Type: application/json' \
+        -d '{"jsonrpc":"2.0","method":"item.get","params":{"output":"extend","filter":{"key_":"net.if.in['$HOST_NIC']","hostid":"'$HOST_ID'"}},"auth":"'$TOKEN'","id":1}' \
+        $ZBX_API_URL \
+        | grep 'jsonrpc' \
+        | jq '.result[].itemid' \
+        | sed 's/[^0-9\|a-Z]*//g')
+
+        ITEM_ID_OUT=$(curl -s -i -X POST \
+        -H 'Content-Type: application/json' \
+        -d '{"jsonrpc":"2.0","method":"item.get","params":{"output":"extend","filter":{"key_":"net.if.out['$HOST_NIC']","hostid":"'$HOST_ID'"}},"auth":"'$TOKEN'","id":1}' \
+        $ZBX_API_URL \
+        | grep 'jsonrpc' \
+        | jq '.result[].itemid' \
+        | sed 's/[^0-9\|a-Z]*//g')
+
+# IN
+        HISTORY_IN=$(curl -s -i -X POST \
+        -H 'Content-Type: application/json' \
+        -d '{"jsonrpc":"2.0","method":"history.get","params":{"itemids":['$ITEM_ID_IN'],"history":3,"output":"extend","time_from":"'$ST_DATE'","time_till":"'$ET_DATE'"},"auth":"'$TOKEN'","id":2}' \
+        $ZBX_API_URL \
+        | grep 'jsonrpc' \
+        | jq '.result[].value' \
+        | sed 's/[^0-9\|a-Z]*//g')
+
+        HISTORY_C_IN=$(echo "$HISTORY_IN" | wc -l)
+        echo "$HISTORY_IN" > $TMP_HISTORY_IN
+        SUM_HISTORY_IN=$(paste -sd+ $TMP_HISTORY_IN | bc)
+        AVG_HISTORY_IN=$(echo "$SUM_HISTORY_IN / $HISTORY_C_IN" | bc)
+
+# OUT
+        HISTORY_OUT=$(curl -s -i -X POST \
+        -H 'Content-Type: application/json' \
+        -d '{"jsonrpc":"2.0","method":"history.get","params":{"itemids":['$ITEM_ID_OUT'],"history":3,"output":"extend","time_from":"'$ST_DATE'","time_till":"'$ET_DATE'"},"auth":"'$TOKEN'","id":2}' \
+        $ZBX_API_URL \
+        | grep 'jsonrpc' \
+        | jq '.result[].value' \
+        | sed 's/[^0-9\|a-Z]*//g')
+
+        HISTORY_C_OUT=$(echo "$HISTORY_OUT" | wc -l)
+        echo "$HISTORY_OUT" > $TMP_HISTORY_OUT
+        SUM_HISTORY_OUT=$(paste -sd+ $TMP_HISTORY_OUT | bc)
+        AVG_HISTORY_OUT=$(echo "$SUM_HISTORY_OUT / $HISTORY_C_OUT" | bc)
+
+# SUM
+        echo "$AVG_HISTORY_IN" > $TMP_HISTORY_SUM
+        echo "$AVG_HISTORY_OUT" >> $TMP_HISTORY_SUM
+        SUM_AVG_HISTORY_OUT_IN=$(paste -sd+ $TMP_HISTORY_SUM | bc)
+
+        SPEED_MB=$(echo "$SUM_AVG_HISTORY_OUT_IN / 8000 / 1000" | bc -l) # Convert to Kilobyte
+        TOTAL_HR=$(echo "$SPEED_MB * 60 * 60" | bc -l) # Convert to Hours
+        TOTAL_DAY=$(echo "$SPEED_MB * 60 * 60 * 24 / 1000" | bc -l) # Convert to DAY
+        TOTAL_MONTH=$(echo "$SPEED_MB * 60 * 60 * 24 / 1000 * 30  / 1000" | bc -l) # Convert to Month
+
+        echo "Host: $HOST_IP  /  NIC: $HOST_NIC"
+        echo "Start Date: $S_DATE  /  End Date: $E_DATE"
+        P_AVS=$(printf "%.2f\n" "$SPEED_MB") && echo "$AVS: $P_AVS"
+        P_TBH=$(printf "%.2f\n" "$TOTAL_HR") && echo "$TBH: $P_TBH"
+        P_TBD=$(printf "%.2f\n" "$TOTAL_DAY") && echo "$TBD: $P_TBD"
+        P_TBM=$(printf "%.2f\n" "$TOTAL_MONTH") && echo "$TBM: $P_TBM"
+        echo "=================="
+
+        echo "$HOST_IP,$HOST_NIC,$P_AVS,$P_TBH,$P_TBD,$P_TBM,$S_DATE,$E_DATE"  >> $BW_RESULT
+
+
+done
